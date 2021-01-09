ec5c92baa99ad0c76ea27ec9535f468f492d6677
Bozhen Liu <april1989@tamu.edu>
2021-01-09 13:41:58 -0600

    update build.sh to build jar; add Manifest.txt

--- build.sh
+++ build.sh
@@ -5,5 +5,10 @@ echo "Building WebLech..."
 echo "Building Spider..."
 javac -classpath lib/log4j-1.1.3.jar -d classes src/weblech/spider/*.java src/weblech/ui/*.java src/weblech/util/*.java
 
+echo "Building Jar..."
+cd classes
+jar cfm weblech.jar ../Manifest.txt weblech/* ../lib 
+mv weblech.jar ../weblech.jar 
+
 echo "Spider built."
 
