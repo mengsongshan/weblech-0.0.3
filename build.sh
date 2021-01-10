8c7d5ef55e8a4d7bcbf3239ef30910ae7465b96d
Bozhen Liu <april1989@tamu.edu>
2021-01-09 19:53:10 -0600

    test 7

--- build.sh
+++ build.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
 
 echo "Building WebLech..."
+export JAVA_HOME=/usr/lib/jvm/java-1.8.0-openjdk-amd64
+java -version
 
 echo "Building Spider..."
 javac -classpath lib/log4j-1.1.3.jar -d classes src/weblech/spider/*.java src/weblech/ui/*.java src/weblech/util/*.java
@@ -8,7 +10,7 @@ javac -classpath lib/log4j-1.1.3.jar -d classes src/weblech/spider/*.java src/we
 echo "Building Jar..."
 cd classes
 jar cfm weblech.jar ../Manifest.txt weblech/* ../lib 
-mv weblech.jar ../weblech.jar 
+mv weblech.jar ../weblech.jar  
 
 echo "Spider built."
 
