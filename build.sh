989f36131c17b26214affd127c227e0b8322f911
Bozhen Liu <april1989@tamu.edu>
2021-01-21 14:37:06 -0600

    test new

--- build.sh
+++ build.sh
@@ -3,7 +3,7 @@
 echo "Building WebLech..."
 
 echo "Building Spider..."
-javac -classpath lib/log4j-1.1.3.jar -d classes src/weblech/spider/*.java src/weblech/ui/*.java src/weblech/util/*.java
+javac -source 1.8 -target 1.8 -classpath lib/log4j-1.1.3.jar -d classes src/weblech/spider/*.java src/weblech/ui/*.java src/weblech/util/*.java
 
 echo "Building Jar..."
 cd classes
